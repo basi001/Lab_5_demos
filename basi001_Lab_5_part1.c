11b3b7b4606c19518c92bc51eeffe0876f896142
basi001 <38081353+basi001@users.noreply.github.com>
2019-10-21 11:44:41 -0700

    2nd commit

--- basi001_Lab_5_part1.c
+++ basi001_Lab_5_part1.c
@@ -1,3 +1,4 @@
+
 /*	Author: basi001
  *  Partner(s) Name: chris hyunh
  *	Lab Section:
@@ -14,78 +15,84 @@
 
 
 
-enum SM_States {Init, Wait, Add, Minus, Reset} SM_State;
+enum SM_States {Begin, Init, Wait, Add, Minus, Reset} SM_State;
 
 //global
-unsigned char tmpC = 0x07;
-#define A0 ~PINA&0x01
-#define A1 ~PINA&0x02
+unsigned char tmpC = 0x00;
+#define inputs (~PINB & 0x03)
 
-
-void SM_func()
+void Abbacus()
 {
 	switch(SM_State){
-		case Init:
+		case Begin:
+			tmpC = 0x00;
 			SM_State = Init;
 			break;
-		case Add:
-			
-		case Minus:
-			
-		case Reset:
-			
-		case Wait:
-			if(A0){
+		case Init:
+			if(inputs == 0x01) {
 				SM_State = Add;
 				break;
-			}
-			else if(A1){
+			} else if(inputs == 0x02) {
 				SM_State = Minus;
-				break; 
-			}
-			else if(A0&&A1){
-				SM_State = Reset; 
+				break;	
+			} else if(inputs == 0x03) {
+				SM_State = Reset;
+				break;
+			} else {
+				SM_State = Init;
 				break;
 			}
-			else {
-				SM_State = Wait; 
+		case Add:
+			SM_State = Wait;
+			break;
+		case Minus:
+			SM_State = Wait;
+			break;
+		case Reset:
+			if((inputs == 0x01) || (inputs == 0x02)) {
+				SM_State = Reset;
+				break;
+			} else {
+				SM_State = Init;
 				break;
 			}
+		case Wait:
+			if((inputs == 0x01) || (inputs == 0x02)) {
+				SM_State = Wait;
+				break;
+			} else {
+				SM_State = Init;
+				break;
+			}
+		default:
+			break;
 	}
 	
 	switch(SM_State){
+		case Begin:
 		case Init:
-			tmpC = 0x07;
-			SM_State = Wait;
-			break;
 		case Wait:
-			tmpC = tmpC;
-			SM_State = Wait;
 			break;
 		case Add:
-			if(tmpC >= 9){
-				tmpC = 0x09;
-				SM_State = Wait;
+			if(tmpC >= 0x09){
+				tmpC = tmpC;
 				break;
 
 			} else {
 				tmpC = tmpC + 0x01;
-				SM_State = Wait;
 				break;
 			}
 		case Minus:
-			if(tmpC <= 0){
-				tmpC = 0x00;
-				SM_State = Wait;
+			if(tmpC <= 0x00){
+				tmpC = tmpC;
 				break;
 			} else {
 				tmpC = tmpC - 0x01;
-				SM_State = Wait;
 				break;
 			}
 		case Reset:
 			tmpC = 0x00;
-			SM_State = Wait;
+			break;
 	}
 	PORTC = tmpC;
 }
@@ -93,12 +100,13 @@ void SM_func()
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
 	DDRC = 0xFF; PORTC = 0x00;
-	SM_State = Init;
+	SM_State = Begin;
     /* Insert your solution below */
     while (1) {
-	SM_func();
+	Abbacus();
     }
     return 1;
 }
+
